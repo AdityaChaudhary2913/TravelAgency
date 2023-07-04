4efb35b6436fb5caeed473a2be1322da3ed5ecef
AdityaTheJaat <programmingID1306@gamil.com>
2023-07-04 10:26:47 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,13 @@ const Navbar = () => {
     <div className='flex md:justify-between items-center'>
       <img src={Jadoo} alt="logo" className="w-[13%]" />
       <div className="flex gap-1 md:gap-20 md:w-auto w-[87%]">
-        <div className="hover:transition-transform hover:text-orange-500">Destinations</div>
-        <div className="hover:transition-transform hover:text-orange-500">Hotels</div>
-        <div className="hover:transition-transform hover:text-orange-500">Flights</div>
-        <div className="hover:transition-transform hover:text-orange-500">Bookings</div>
-        <div className="hover:transition-transform hover:text-orange-500">Login</div>
-        <div className="hover:transition-transform hover:text-orange-500">Signup</div>
-        <div className='hover:transition-transform hover:text-orange-500'>EN</div>
+        <div className="hover:transition-transform hover:text-orange-500 hover:cursor-pointer">Destinations</div>
+        <div className="hover:transition-transform hover:text-orange-500 hover:cursor-pointer">Hotels</div>
+        <div className="hover:transition-transform hover:text-orange-500 hover:cursor-pointer">Flights</div>
+        <div className="hover:transition-transform hover:text-orange-500 hover:cursor-pointer">Bookings</div>
+        <div className="hover:transition-transform hover:text-orange-500 hover:cursor-pointer">Login</div>
+        <div className="hover:transition-transform hover:text-orange-500 hover:cursor-pointer">Signup</div>
+        <div className='hover:transition-transform hover:text-orange-500 hover:cursor-pointer'>EN</div>
       </div>
     </div>
   )
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -10,22 +10,22 @@ const Services = () => {
       <div className='text-center text-[#5E6282]'>CATEGORY</div>
       <div className='text-center font-bold text-[50px]'>We Offer Best Services</div><br/><br/>
       <div className='flex flex-wrap justify-center items-center p-3 text-[#5E6282] text-center mx-auto'>
-        <div className='p-5 md:w-[25%] hover:border-2 hover:rounded-xl hover:bg-slate-100'>
+        <div className='p-5 md:w-[25%] hover:border-2 hover:rounded-xl hover:bg-slate-100 hover:transition-colors'>
           <img src={s1} className='mx-auto'/>
           <div>Calculated Weather</div>
           <div>Built Wicket longer admire do barton vanity itself do in it.</div>
         </div>
-        <div className='p-5 md:w-[25%] hover:border-2 hover:rounded-xl hover:bg-slate-100'>
+        <div className='p-5 md:w-[25%] hover:border-2 hover:rounded-xl hover:bg-slate-100 hover:transition-colors'>
           <img src={s2} className='w-[110px] mx-auto'/>
           <div>Best Flights</div>
           <div>Engrossed listening. Park gate sell they west hard for the.</div>
         </div>
-        <div className='p-5 md:w-[25%] hover:border-2 hover:rounded-xl hover:bg-slate-100'>
+        <div className='p-5 md:w-[25%] hover:border-2 hover:rounded-xl hover:bg-slate-100 hover:transition-colors'>
           <img src={s3} className='w-[100px] mx-auto'/>
           <div>Local Events</div>
           <div>Barton vanity itself do in it. Preferd to men it engrossed listening. </div>
         </div>
-        <div className='p-5 md:w-[25%] hover:border-2 hover:rounded-xl hover:bg-slate-100'>
+        <div className='p-5 md:w-[25%] hover:border-2 hover:rounded-xl hover:bg-slate-100 hover:transition-colors'>
           <img src={s4} className='mx-auto'/>
           <div>Customization</div>
           <div>We deliver outsourced aviation services for military customers</div>
